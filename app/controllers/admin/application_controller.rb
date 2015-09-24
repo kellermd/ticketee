f7d1d19c9ee13028106221c7fd5cf92e18682735
David Keller <kellermd@gmail.com>
2015-09-23 20:20:25 -0400

    Handle authorization errors gracefully

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ApplicationController < ApplicationController
   before_action :authorize_admin!
 
+
   def index
   end
 
@@ -11,4 +12,5 @@ class Admin::ApplicationController < ApplicationController
       redirect_to root_path, alert: "You must be an admin to do that."
     end
   end
+  
 end
